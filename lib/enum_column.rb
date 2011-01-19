1f5438862d6d72742c344ea7b5b99a0758fb2dd0
Narihiro Nakamura <authornari@gmail.com>
2011-01-19 19:31:46 +0900

    support alias column name. update document.

--- lib/enum_column.rb
+++ lib/enum_column.rb
@@ -4,25 +4,51 @@ module EnumColumn
   end
 
   module ClassMethods
-    def enum_column(column, *options)
+
+    # Define enum column to model.
+    #
+    # ==== Example
+    #   enum_column :visibility, :privated, :friend_only, :publiced
+    #   # Use alias column name.
+    #   enum_column [:v, :visibility], :privated, :friend_only, :publiced
+    #   # Change enum values. VISIBILITIES[:privated]) #=> 10
+    #   enum_column :visibility, {privated: 10, friend_only: 11, publiced: 12}
+    #
+    # Define VISIBILITY constant.
+    #
+    #   Klass::VISIBILITIES[:privated] #=> 1
+    #
+    # Define methods.
+    #
+    #  # if visibility == VISIBILITIES[:privated]
+    #  visibility_privated? #=> true
+    #  visibility_friend_only? #=> false
+    #  visibility_publiced? #=> false
+    #  visibility_name #=> "privated"
+    def enum_column(names, *options)
       enum = ActiveSupport::OrderedHash.new
       if options.first.is_a?(Hash)
         options.first.sort_by{|_, v| v.to_s.to_i}.each{|k, v| enum[k.to_sym] = v}
       else
         options.each_with_index{|k, i| enum[k.to_sym] = i+1}
       end
-      const_name = column.to_s.pluralize.upcase
+      if names.is_a?(Array)
+        name, column = names
+      else
+        name = column = names
+      end
+      const_name = name.to_s.pluralize.upcase
       self.const_set(const_name, enum.freeze)
 
       enum.each do |key, value|
         class_eval <<-METHOD
-          def #{column}_#{key}?; #{column} == #{const_name}[:#{key}]; end
+          def #{name}_#{key}?; self[:#{column}] == #{const_name}[:#{key}]; end
         METHOD
       end
 
       class_eval <<-METHO
-        def #{column}_name
-          #{const_name}.invert[#{column}]
+        def #{name}_name
+          #{const_name}.invert[self[:#{column}]]
         end
       METHO
     end
